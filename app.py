f5e65419fd1ee96cfd87eee437441a92deff84e2
Daniel Trnka <daniel.trnka@gmail.com>
2016-07-31 12:54:51 +0200

    Rewritted to QGraphicsScene for better performance

--- app.py
+++ app.py
@@ -1,86 +1,88 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QPoint
-from PyQt5.QtGui import QColor, QPolygon
+from PyQt5.QtGui import QColor
 from PyQt5.QtGui import QPainter, QFont
 from PyQt5.QtGui import QPainterPath
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtGui import QPolygonF
+from PyQt5.QtWidgets import QGraphicsScene
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
 
 from xdot import *
 
 
 class MyParser(XDotParser):
-    def handle_edge(self, src_id, dst_id, attrs):
-        super().handle_edge(src_id, dst_id, attrs)
+	def handle_edge(self, src_id, dst_id, attrs):
+		super().handle_edge(src_id, dst_id, attrs)
 
 
-class Widget(QtWidgets.QWidget):
-    def __init__(self, flags, *args, **kwargs):
-        super().__init__(flags, *args, **kwargs)
+class Widget(QtWidgets.QGraphicsView):
+	def __init__(self):
+		super().__init__()
 
-        parser = MyParser(open("a.xdot").read().encode('utf-8'))
-        self.graph = parser.parse()
+		parser = MyParser(open("a.xdot").read().encode('utf-8'))
+		self.graph = parser.parse()
 
-    def paintEvent(self, QPaintEvent):
-        super().paintEvent(QPaintEvent)
+		self.p = p = QGraphicsScene(self)
+		self.setRenderHints(QPainter.Antialiasing | QPainter.SmoothPixmapTransform | QPainter.HighQualityAntialiasing)
 
-        p = QPainter(self)
-        p.setRenderHint(QPainter.Antialiasing)
-        p.setWindow(0, 0, self.width(), self.height())
-        p.setViewport(0, 0, self.width(), self.height())
-        p.begin(self)
-        p.setPen(QColor(168, 34, 3))
-        p.setBrush(QColor(168, 34, 3))
+		self.setScene(self.p)
+		self.show()
 
-        def toQColor(color):
-            return QColor(color[0] * 255, color[1] * 255, color[2] * 255, color[3] * 255)
+		def toQColor(color):
+			return QColor(color[0] * 255, color[1] * 255, color[2] * 255, color[3] * 255)
 
-        for type in [self.graph.nodes, self.graph.edges]:
-            for node in type:
-                for shape in node.shapes:
-                    if isinstance(shape, EllipseShape):
-                        if shape.filled:
-                            p.setBrush(toQColor(shape.pen.fillcolor))
-                        else:
-                            p.setBrush(QColor(0, 0, 0, 0))
-                        p.drawEllipse(shape.x0 - shape.w / 2.0, shape.y0 - shape.h / 2.0, shape.w, shape.h)
-                    elif isinstance(shape, TextShape):
-                        p.setFont(QFont(shape.pen.fontname, shape.pen.fontsize))
-                        p.drawText(shape.x, shape.y, shape.t)
-                    elif isinstance(shape, BezierShape):
-                        path = QPainterPath()
-                        path.moveTo(shape.points[0][0], shape.points[0][1])
+		for type in [self.graph.nodes, self.graph.edges]:
+			for node in type:
+				for shape in node.shapes:
+					if isinstance(shape, EllipseShape):
+						e = p.addEllipse(shape.x0 - shape.w / 2.0, shape.y0 - shape.h / 2.0, shape.w, shape.h)
 
-                        for part in shape.points:
-                            path.lineTo(part[0], part[1])
+						if shape.filled:
+							e.setBrush(toQColor(shape.pen.fillcolor))
+					elif isinstance(shape, TextShape):
+						p.setFont(QFont(shape.pen.fontname, shape.pen.fontsize))
+						text = p.addText(shape.t)
+						text.setPos(shape.x, shape.y)
+					elif isinstance(shape, BezierShape):
+						path = QPainterPath()
+						path.moveTo(shape.points[0][0], shape.points[0][1])
 
-                        p.setBrush(QColor(0,0,0,0))
-                        p.drawPath(path)
-                    elif isinstance(shape, PolygonShape):
-                        polygon = QPolygon()
-                        for point in shape.points:
-                            polygon.append(QPoint(point[0], point[1]))
-                        p.drawPolygon(polygon)
-                    else:
-                        print("unknown")
+						for part in shape.points:
+							path.lineTo(part[0], part[1])
 
+						p.addPath(path)
+					elif isinstance(shape, PolygonShape):
+						polygon = QPolygonF()
+						for point in shape.points:
+							polygon.append(QPoint(point[0], point[1]))
+						p.addPolygon(polygon, QColor(0, 0, 0))
+					else:
+						print("unknown")
 
-        #p.drawLine(1, 1, 100, 100)
-        #p.drawLine(10, 1, 100, 100)
-        #p.drawEllipse(10, 10, 10, 10)
-        #p.drawEllipse(81, 41, 10, 10)
-        p.end()
+						# self.fitInView(p.itemsBoundingRect(), Qt.Qt.KeepAspectRatio)
 
 
 class ExampleApp(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        super(ExampleApp, self).__init__(parent)
-        w = Widget(self)
-        w.setGeometry(0, 0, self.width(), self.height())
+	def __init__(self, parent=None):
+		super(ExampleApp, self).__init__(parent)
+
+		frame = QWidget()
+		layout = QVBoxLayout(frame)
+		frame.show()
+
+		layout.addWidget(Widget())
+
+		self.setCentralWidget(frame)
+
 
 def main():
-    app = QtWidgets.QApplication([])
-    window = ExampleApp()
-    window.show()
-    app.exec_()
+	app = QtWidgets.QApplication([])
+	window = ExampleApp()
+	window.setWindowTitle('GUI')
+	window.setFixedSize(800, 600)
+	window.show()
+	app.exec_()
+
 
 main()
