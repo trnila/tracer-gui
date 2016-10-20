9ee4af41b3b57c579d8fbd01a173a1da8d5d7f2d
Daniel Trnka <daniel.trnka@gmail.com>
2016-10-20 20:02:24 +0200

    Data are passed to graph though bag

--- DotWriter.py
+++ DotWriter.py
@@ -1,7 +1,21 @@
+class Bag:
+    def __init__(self):
+        self.id = 0
+        self.data = {}
+
+    def write(self, data):
+        self.data[self.id] = data
+        self.id += 1
+        return self.id - 1
+
+    def get(self, id):
+        return self.data[id]
+
 class DotWriter:
     def __init__(self, out):
         self.out = out
         self.subgraph_id = 0
+        self.bag = Bag()
 
     def begin(self):
         self.out.write("digraph G {\n")
@@ -20,7 +34,10 @@ class DotWriter:
     def write_node(self, id, title):
         self.out.write('\t"%s" [label="%s"];\n' % (self._escape(id), self._escape(title)))
 
-    def write_edge(self, src, dst, **kwargs):
+    def write_edge(self, src, dst, data=None, **kwargs):
+        if data is not None:
+            kwargs['data_id'] = self.bag.write(data)
+
         args = ", ".join(["%s=\"%s\"" % (i, self._escape(j)) for i, j in kwargs.items()])
         self.out.write('\t"%s" -> "%s" [%s];\n' % (self._escape(src), self._escape(dst), args))
 
@@ -30,3 +47,4 @@ class DotWriter:
 
     def _escape(self, s):
         return str(s).replace('"', '\\"')
+
