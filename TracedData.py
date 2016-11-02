881e64f05bb7b9d2247b3707fc9df80dca031820
Daniel Trnka <daniel.trnka@gmail.com>
2016-11-02 21:13:12 +0100

    Show mmaped data

--- TracedData.py
+++ TracedData.py
@@ -155,13 +155,23 @@ class Mmap(Des):
         )
 
     def gui(self, window):
+        file = open(self.descriptor['path'], 'rb')
         def _format_mmap(item):
-            return "0x%X - 0x%X (%s) %s %s" % (
+            def r(range):
+                start, stop = range.split('-', 2)
+                start = int(start, 16) - item['address']
+                stop = int(stop, 16) - item['address']
+
+                file.seek(start)
+                return file.read(stop - start).decode('utf-8', 'ignore')
+
+            return "0x%X - 0x%X (%s) %s %s %s" % (
                 item['address'],
                 item['address'] + item['length'],
                 utils.format_bytes(item['length']),
                 maps.mmap_prots.format(item['prot']),
                 maps.mmap_maps.format(item['flags']),
+                [r(i) for i in item['regions']]
             )
 
         value = "\n".join(map(_format_mmap, self.descriptor['mmap']))
