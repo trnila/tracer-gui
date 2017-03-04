355edeab85ccfafc5704b5b008a498fd50a36ca1
Daniel Trnka <daniel.trnka@gmail.com>
2017-03-04 18:31:35 +0100

    Dont crash if regions are empty

--- tracergui/actions/process_created.py
+++ tracergui/actions/process_created.py
@@ -80,7 +80,7 @@ class ProcessCreated(Action):
         window.addTab(cmdline, "Command")
         window.addTab(table, "Environments")
 
-        if 'regions' in self.process:
+        if 'regions' in self.process and self.process['regions']:
             mmaps = QTableWidget()
             mmaps.setColumnCount(2)
             mmaps.setHorizontalHeaderItem(0, QTableWidgetItem("Variable"))
