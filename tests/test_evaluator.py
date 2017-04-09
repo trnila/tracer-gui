59453d97c21e0fba92a01d8c669707a456c4a132
Daniel Trnka <daniel.trnka@gmail.com>
2017-04-09 11:48:55 +0200

    Fixed tests

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -7,16 +7,17 @@ from tracergui.objects.system import System
 
 
 class TestEvaluator(TestCase):
+    def setUp(self):
+        self.sys = System("/tmp", {'processes': {}})
+
     def test_true(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
 
         self.assertTrue(evalme("True", process=proc))
         self.assertFalse(evalme("False", process=proc))
 
     def test_is_file_exact_match(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
         desc = Descriptor(proc, {'type': 'file', 'path': '/etc/hosts'})
 
         self.assertTrue(evalme("is_file('/etc/hosts')", descriptor=desc))
@@ -25,24 +26,21 @@ class TestEvaluator(TestCase):
         self.assertFalse(evalme("is_file(exactmatch('/etc/passwd'))", descriptor=desc))
 
     def test_is_file_contains(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
         desc = Descriptor(proc, {'type': 'file', 'path': '/lib/libc.so.1'})
 
         self.assertTrue(evalme("is_file(contains('.so'))", descriptor=desc))
         self.assertFalse(evalme("is_file(contains('.dll'))", descriptor=desc))
 
     def test_is_file_startswith(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
         desc = Descriptor(proc, {'type': 'file', 'path': '/lib/libc.so.1'})
 
         self.assertTrue(evalme("is_file(startswith('/lib'))", descriptor=desc))
         self.assertFalse(evalme("is_file(startswith('/usr/lib'))", descriptor=desc))
 
     def test_is_file_not(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
         desc = Descriptor(proc, {'type': 'file', 'path': '/lib/libc.so.1'})
 
         self.assertFalse(evalme("is_file(Not(contains('.so')))", descriptor=desc))
@@ -53,8 +51,7 @@ class TestEvaluator(TestCase):
         self.assertTrue(evalme("is_file(Not(Not(contains('.so'))))", descriptor=desc))
 
     def test_is_file_and(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
         desc = Descriptor(proc, {'type': 'file', 'path': '/lib/libc.so.1'})
 
         self.assertTrue(evalme("is_file(And(contains('.so'), contains('lib')))", descriptor=desc))
@@ -63,8 +60,7 @@ class TestEvaluator(TestCase):
         self.assertFalse(evalme("is_file(contains('.so') & contains('dll'))", descriptor=desc))
 
     def test_is_file_or(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
         desc = Descriptor(proc, {'type': 'file', 'path': '/lib/libc.so.1'})
 
         self.assertTrue(evalme("is_file(Or(contains('.so'), contains('dll')))", descriptor=desc))
@@ -74,8 +70,7 @@ class TestEvaluator(TestCase):
         self.assertFalse(evalme("is_file(contains('x') | contains('z'))", descriptor=desc))
 
     def test_is_file_complex(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
         desc = Descriptor(proc, {'type': 'file', 'path': '/lib/libc.so.1'})
 
         self.assertTrue(evalme("is_file(contains('.so') & (startswith('/lib') & ~contains('dll')) | endswith('.dll'))",
@@ -84,8 +79,7 @@ class TestEvaluator(TestCase):
                                 descriptor=desc))
 
     def test_use_and_or(self):
-        sys = System("/tmp", {})
-        proc = Process(sys, {'descriptors': []})
+        proc = Process(self.sys, {'descriptors': []})
         desc = Descriptor(proc, {'type': 'file', 'path': '/lib/libc.so.1'})
 
         for expr in ["is_file(not contains('.so'))", "is_file(contains('.so') and contains('.so'))",
